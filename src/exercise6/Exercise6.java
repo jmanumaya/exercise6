24b517276e32b8c1c603dbe287e4ccaac7637b24
jm.maya <jm.maya@106-20.info2>
2024-09-20 14:20:16 +0200

    Ejercicio 6 terminado

---
+++ src/exercise6/Exercise6.java
@@ -0,0 +1,13 @@
+package exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Jose Manuel");
+		System.out.println("Address: Calle Quevedo Nº 11º\n");
+		// Puedo no poner el \n y optar por System.out.println() en esta línea;
+		System.out.println("Telephone Number: 629263572");
+
+	}
+
+}
